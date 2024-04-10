9b44276df58557348ab8975d3515ab3e4b45af85
0End-Of-Light0 <125026630+0End-Of-Light0@users.noreply.github.com>
2024-04-10 12:42:21 +0300

    Update some.js

--- some.js
+++ some.js
@@ -1,34 +1,36 @@
+
 window.onload = function () {
   Currency()
 };
 const Currency = () => {
   readTextFile("./json/data.json", function(text){
     var data = JSON.parse(text);
-    var count = Object.keys(data).length;
+    //var count = Object.keys(data).length;
     for (let i = 0; i < data.arr.length; i++) {
-      const element =  document.querySelector('.block' + [i])
-      const classList = element.classList[0]
-      console.log(classList);
-      if (data[i].curp.indexOf(',') === -1) {
+      //const element =  document.querySelector('.block' + i)
+      //const classList = element.classList[0]
+      //console.log(classList);
+      if (data.arr[i].curp.indexOf(',') === -1) {
         try {
           const response = fetch('https://b1d9-2a01-4f9-2a-427-00-2.ngrok-free.app/test/', {
             method: 'GET',
             headers: {
               'Content-Type': 'application/json',
-              'currency-pair': data[i].curp,
-              'period': data[i].per,
-              'interval': data[i].int,
+              'currency-pair': data.arr[i].curp,
+              'period': data.arr[i].per,
+              'interval': data.arr[i].int,
               'ngrok-skip-browser-warning': 'true'
             }
           });
-          const res = "<div id=\"Currency\"><h2>Валюта: " + `${response.curp.Name} + "</h2><p>Текущий курс:" + ${response.curp.Close} + "</p></div>"`;
-          document.querySelector('.block' + [i]).innerHTML = res;
+          const res = "<div id=\"Currency\"><h2>Валюта: " + `${response.arrs[0].Name} + "</h2><p>Текущий курс:" + ${response.arrs[0].Close} + "</p></div>"`;
+          document.querySelector('.block' + i).innerHTML = res;
         } catch (error) {
           console.error(error);
         }
       }
       else {
-        const listcur = data[i].curp.split(',');
+        const listcur = data.arr[i].curp.split(',');
+        console.log(listcur);
         let exp = "<div></div>"
         for (let k = 0; k < listcur.length; k++) {
           try {
@@ -42,11 +44,11 @@ const Currency = () => {
               'ngrok-skip-browser-warning': 'true'
             }
             });
-            exp +="<div id=\"Currency\"><h2>Валюта: " + `${response.listcur[k].Name} + "</h2><p>Текущий курс:" + ${response.listcur[k].Close} + "</p></div>"`;
+            exp +="<div id=\"Currency\"><h2>Валюта: " + `${response.arrs[0].Name} + "</h2><p>Текущий курс:" + ${response.arrs[0].Close} + "</p></div>"`;
           } catch (error) {
             console.error(error);
           }
-        document.querySelector('.block' + [i]).innerHTML = exp;
+        document.querySelector('.block' + i).innerHTML = exp;
         }
       }
     }
